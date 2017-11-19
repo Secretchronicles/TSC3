d08c70560fb868b0a68e9d8d3bdf9d9d2fdbf2f0
Marvin Gülker <m-guelker@phoenixmail.de>
2017-11-19 15:32:49 +0100

    Add copyright notices.

--- tscproc/genpng.cpp
+++ tscproc/genpng.cpp
@@ -1,3 +1,20 @@
+/* TSC is a two-dimensional jump’n’run platform game.
+ * Copyright © 2017 The TSC Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "genpng.hpp"
 #include "commandline.hpp"
 #include "util.hpp"
