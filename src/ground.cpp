ad475e7be154eabe823b2e830769c8fc9a818efd
Marvin Gülker <m-guelker@phoenixmail.de>
2018-03-31 13:15:02 +0200

    Add tileset XML settings parser.

--- src/ground.cpp
+++ src/ground.cpp
@@ -1,33 +1,102 @@
 #include "ground.hpp"
 #include "pathmap.hpp"
 #include "util.hpp"
+#include "xerces_helpers.hpp"
 #include <pathie/path.hpp>
+#include <xercesc/util/PlatformUtils.hpp>
+#include <xercesc/sax2/DefaultHandler.hpp>
+#include <xercesc/sax2/SAX2XMLReader.hpp>
+#include <xercesc/sax2/Attributes.hpp>
+#include <xercesc/sax2/XMLReaderFactory.hpp>
 #include <stdexcept>
+#include <vector>
+#include <tuple>
 
 using namespace TSC;
 using namespace std;
 using Pathie::Path;
+using namespace xercesc;
+
+namespace {
+    // Tilset XML settings handler.
+    class TilesetSettingsHandler: public xercesc::DefaultHandler
+    {
+    public:
+        TilesetSettingsHandler()
+            {
+            }
+        void startElement(const XMLCh* const,
+                          const XMLCh* const xlocalname,
+                          const XMLCh* const,
+                          const Attributes& attrs)
+            {
+                string localname(X2U(xlocalname));
+                m_chars.clear();
+
+                if (localname == "colrect") {
+                    string x = X2U(attrs.getValue(U2X("x")));
+                    string y = X2U(attrs.getValue(U2X("y")));
+                    string w = X2U(attrs.getValue(U2X("width")));
+                    string h = X2U(attrs.getValue(U2X("height")));
+
+                    bboxes.emplace_back(stoi(x), stoi(y), stoi(w), stoi(h));
+                }
+            }
+
+        void endElement(const XMLCh* const,
+                        const XMLCh* const xlocalname,
+                        const XMLCh* const)
+            {
+                string localname(X2U(xlocalname));
+
+                if (localname == "rows") {
+                    rows = stoi(m_chars);
+                }
+                else if (localname == "cols") {
+                    cols = stoi(m_chars);
+                }
+            }
+
+        void characters(const XMLCh* const chars,
+                        const XMLSize_t)
+            {
+                m_chars += X2U(chars);
+            }
+
+        int rows;
+        int cols;
+        vector<tuple<int, int, int, int>> bboxes;
+    private:
+        string m_chars;
+    };
+}
 
 /**
  * Construct a new level ground object. The `tileset` is a path
  * relative to TSC's data/pixmaps/tilesets directory.
  */
 Ground::Ground(const string& tileset)
+    : m_rows(0),
+      m_cols(0)
 {
-    Path path = Pathmap::GetPixmapsPath() / "tilesets" / tileset;
-    if (!path.exists())
-        throw(runtime_error(format("Tileset '%s' does not exist", path.utf8_str())));
+    Path tileset_path = Pathmap::GetPixmapsPath() / "tilesets" / tileset;
+    if (!tileset_path.exists())
+        throw(runtime_error(format("Tileset '%s' does not exist", tileset_path.utf8_str())));
+
+    Path settings_path = tileset_path.sub_ext(".xml");
+    if (!settings_path.exists())
+        throw(runtime_error(format("Tileset settings file '%s' not found", settings_path.utf8_str())));
 
     sf::Image img;
-    img.loadFromFile(path2sf(path));
+    img.loadFromFile(path2sf(tileset_path));
 
     // SFML docs say that 512px are minimum of quite old cards,
     // and say that newer cards support 8192px or more.
     unsigned int maxedge = sf::Texture::getMaximumSize();
     if (img.getSize().x > maxedge)
-        throw(runtime_error(format("Tileset '%s' is too large for your graphics card, which only supports up to %d pixels for an edge!", path.utf8_str(), maxedge)));
+        throw(runtime_error(format("Tileset '%s' is too large for your graphics card, which only supports up to %d pixels for an edge!", tileset_path.utf8_str(), maxedge)));
     if (img.getSize().y > maxedge)
-        throw(runtime_error(format("Tileset '%s' is too large for your graphics card, which only supports up to %d pixels for an edge!", path.utf8_str(), maxedge)));
+        throw(runtime_error(format("Tileset '%s' is too large for your graphics card, which only supports up to %d pixels for an edge!", tileset_path.utf8_str(), maxedge)));
 
     m_tileset.loadFromImage(img);
 
