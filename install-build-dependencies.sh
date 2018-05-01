0f2a2fbca9dbf26afb7a5e304530f98cc010c9bb
Marvin Gülker <m-guelker@phoenixmail.de>
2018-05-01 18:21:42 +0200

    Curate dependency list

--- install-build-dependencies.sh
+++ install-build-dependencies.sh
@@ -3,10 +3,5 @@
 # Requires Debian 9 or Ubuntu 16.10 or newer
 
 # Install deb packages
-sudo apt -y install ruby-full rake gperf pkg-config bison libglew-dev \
-  freeglut3-dev gettext libpng-dev libpcre3-dev libxml++2.6-dev \
-  libfreetype6-dev libdevil-dev libboost-all-dev libsfml-dev \
-  libcegui-mk2-dev cmake build-essential git git-core libxerces-c-dev
-
-# Install Ruby gems
-sudo gem install bundler nanoc adsf kramdown coderay
+sudo apt -y install cmake build-essential git pkg-config \
+     gettext zlib1g-dev libpng-dev libsfml-dev libxerces-c-dev
